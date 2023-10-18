fe2675186db4ac7de233653fffffd2a07073a379
NandiniEliti <138872027+NandiniEliti@users.noreply.github.com>
2023-10-18 10:36:11 +0530

    Update ReverseWord.js

--- ReverseWord.js
+++ ReverseWord.js
@@ -1,20 +1,12 @@
 function reverseWordsInSentence(sentence) {
-    // Split the sentence into words
     const words = sentence.split(' ');
-  
-    // Reverse each word and store them in an array
     const reversedWords = words.map(word => {
-      // Use split and reverse to reverse the characters in the word
       return word.split('').reverse().join('');
     });
-  
-    // Join the reversed words back into a sentence
     const reversedSentence = reversedWords.join(' ');
   
     return reversedSentence;
   }
-  
-  // Example usage:
-  const inputSentence = "Hello World";
+  const inputSentence = "This is a sunny day";
   const reversedSentence = reverseWordsInSentence(inputSentence);
   console.log(reversedSentence); 
